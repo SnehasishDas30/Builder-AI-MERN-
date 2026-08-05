3dddf7f94e39f376968da84d24042b124d49cb86
SnehasishDas30 <sid29082001@gmail.com>
2026-08-05 13:48:15 +0530

    created user authentication froms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import {GuestLayout , AuthLayout} from './pages/Layout'
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
@@ -22,6 +22,9 @@ const App = () => {
         <Route path='/preview/:id' element={<PreviewPage/>}/>
 
       </Route>
+
+      {/* Catch-all */}
+         <Route path='*' element={<Navigate to="/" replace />}/>
 
     </Routes>
   )
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,13 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import api from "../api/api"
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({children}){
+
+    const navigate = useNavigate()
 
     //Auth States
 
@@ -27,10 +31,43 @@ useEffect(()=>{
     checkSession()
 },[checkSession])
 
+const login = async (email, password) => {
+    try {
+        const { data } = await api.post("/api/auth/login",
+        {email, password});
+        setUser(data.user)
+        toast.success("Welcome back!")
+        navigate("/")
+    } catch (error) {
+        console.log("Login Failed: ",error);
+        const errMsg = err?.response?.data?.error || "Invalidemail or password";
+        toast.error(errMsg);
+        throw new Error(errMsg);
+
+    }
+}
+
+const register = async (name, email, password) => {
+    try {
+        const { data } = await api.post("/api/auth/register",
+        {name, email, password});
+        setUser(data.user)
+        toast.success("Account Created Successfully!")
+        navigate("/")
+    } catch (error) {
+        console.log("Registration Failed: ",error);
+        const errMsg = error?.response?.data?.error || "Registration Failed";
+        toast.error(errMsg);
+        throw new Error(errMsg);
+
+    }
+}
     return (
         <AppContext.Provider value={{
             user,
-            loadingUser
+            loadingUser,
+            login,
+            register
         }}>
             {children}
         </AppContext.Provider>
